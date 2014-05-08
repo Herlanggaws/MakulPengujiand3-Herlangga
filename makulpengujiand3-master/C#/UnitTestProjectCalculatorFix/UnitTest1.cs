bb271fcaf61a5fcade639a89bc7bf034784c830e
Herlanggaws <herlangga.wicaksono@gmail.com>
2014-05-08 14:17:35 +0700

    Tugas Pengujian Aplikasi
    Tugas Pengujian Aplikasi

---
+++ makulpengujiand3-master/C#/UnitTestProjectCalculatorFix/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculatorv1;
+
+namespace UnitTestProjectCalculatorFix
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodPenjumlahan()
+        {
+            MainWindow mW = new MainWindow();
+            double a = 1;
+            double b = 2;
+            double hasil = 3;
+            double actual = mW.jumlah(a, b);
+            Assert.AreEqual<double>(hasil,actual,"Integer Salah");
+
+        }
+
+        [TestMethod]
+        public void TestMethodPengurangan()
+        {
+            MainWindow mW = new MainWindow();
+            double a = 3;
+            double b = 2;
+            double hasil = 1;
+            double actual = mW.kurang(a, b);
+            Assert.AreEqual<double>(hasil, actual, "Integer Salah");
+
+        }
+
+        [TestMethod]
+        public void TestMethodPerkalian()
+        {
+            MainWindow mW = new MainWindow();
+            double a = 1;
+            double b = 2;
+            double hasil = 2;
+            double actual = mW.kali(a, b);
+            Assert.AreEqual<double>(hasil, actual, "Integer Salah");
+
+        }
+
+        [TestMethod]
+        public void TestMethodPembagian()
+        {
+            MainWindow mW = new MainWindow();
+            double a = 4;
+            double b = 2;
+            double hasil = 2;
+            double actual = mW.bagi(a, b);
+            Assert.AreEqual<double>(hasil, actual, "Integer Salah");
+
+        }
+    }
+}
